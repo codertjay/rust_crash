13d21bf05b40b282dee4a5da70a301742895d3f8
Codertjay <codertjay@gmail.com>
2022-05-23 20:55:42 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+
+
+fn main() {
+  /*   let a = 10;
+    let b = 110;
+    println!("Hello, world! a={}, b={}",a,b); 
+
+    let unsigned  : u8= 10;
+    let signed  : i8= -10;
+
+    let float: f32= 1.2; 
+    println!("unsign {}, sign {} float={}", unsigned, signed, float);
+
+    let letter = "c";
+    let emoji = "\u{1F600}";
+    println!("letter {}, emoji {}", letter, emoji);
+    let is_true:bool = true;
+    println!("IsTrue: {}", is_true);
+
+    // array
+
+    let arr: [u8;3] = [1,2,3];
+    let other_arr = [100,5];
+
+    println!("Index: {}, length: {}", arr[0], other_arr.len());
+
+    println!("{:?}",other_arr)
+
+
+    // tuples
+
+    let tuple: (u8,bool,f32) = (5,true,10.2);
+
+    let tuple2 = (3,5);
+    println!("First {}, second {}", tuple.0, tuple2.1);
+    println!("{:?}",tuple2)
+
+
+    // function 
+    println!("{}",is_even(2));
+
+    pub fn is_even(num: u8) -> bool {
+        let digit = num % 2;
+        // return value below 
+        digit ==0 // retun bool
+
+    }
+
+// immutability
+
+let mut num_ = 5;
+num_ = 3;
+println!("{}",num_);
+
+//  arrays slices
+
+let arr = [1,3,5,2,6]; // length
+let slice= &arr[1.. 3]; // [3, 5]  dont know the length
+println!("{:?}",slice)
+
+
+
+// strings
+let str: &str = " Helo world";
+println!("{:?}",str);
+
+let mut string : String = String::from("Hello world "); //
+println!("{:?}",string);
+
+let _slice = &string[..6];
+let _ =_slice.len() == 6;
+string.push('1');
+string.push_str("! Bob");
+string = string.replace("Hello","Bye ");
+println!("{:?}",string);
+
+
+
+let n= -1;
+
+if n > 0 {
+    println!("Greater than 0");
+}else if n < 0 {
+    println!("Less than 0");
+}else{
+    println!("Not greater than or Less than 0");
+}
+
+
+
+// for loop
+for i in 0..5{
+    println!("{:?}",i);
+}
+
+
+//  while loop
+
+let mut i = 0 ;
+while i < 5{
+    println!("{:?}",i);
+    i = i + 1;
+    if i == 3 {
+        println!("exit");
+        break;
+    }
+}
+
+*/
+
+//  match 
+
+let i = 5;
+match i {
+    0 -> println!("0")
+}
+
+
+
+
+
+}
+
+
